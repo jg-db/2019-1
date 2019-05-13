e35b18948472c2f71bdedad78de9d04fa52275e2
jg-db <50572893+jg-db@users.noreply.github.com>
2019-05-13 11:03:45 +0300

    Add files via upload

---
+++ l2-createUser.sql
@@ -0,0 +1,6 @@
+-- Commands to be run in IDE
+alter session set "_ORACLE_SCRIPT"=true;
+grant connect to working
+grant connect, resource, dba to working;
+grant create session to working;
+grant unlimited tablespace to working;
