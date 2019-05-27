00c21d212945bc714924f832dfffe28f298b84e0
jg-db <50572893+jg-db@users.noreply.github.com>
2019-05-27 18:30:45 +0300

    Update l2-createUser.sql

--- l2-createUser.sql
+++ l2-createUser.sql
@@ -1,5 +1,6 @@
 -- Commands to be run in IDE
 alter session set "_ORACLE_SCRIPT"=true;
+create user working identified by working;
 grant connect to working
 grant connect, resource, dba to working;
 grant create session to working;
