827809dcf99946351de10f14e4c4a7b6d8ddcf92
jg-db <50572893+jg-db@users.noreply.github.com>
2019-06-10 07:39:19 +0300

    Update l5-scenario13.sql

--- l5-scenario13.sql
+++ l5-scenario13.sql
@@ -6,7 +6,7 @@ ADD DISCOUNT number;
 2. Update discount, set discount = 20 percents to each customer with at least one SHIPPED order
 
 UPDATE xx_customers xc
-set cx.discount = 20
+set discount = 20
 where EXISTS
 (
  select 1
@@ -19,14 +19,14 @@ where EXISTS
 
 UPDATE xx_customers
 set discount = 0
-where discount = null;
+where discount is null;
 
 4. COMMIT changes
 COMMIT;
 
 5. Check that update was done correctly
 select *
-from xx_customers
+from xx_customers;
 
 
 6. select all orders and show amounts with discount (if applicable) and without a discount (USING case)
@@ -43,7 +43,7 @@ where xo.customer_id = xc.customer_id;
 
 7. select all orders and show amounts with discount (if applicable) and without a discount (USING DECODE)
 select xc.customer_name, xo.order_number, xo.total_amount,
-     DECODE(xc.discount,0, xo.total_amount, xo.total_amount * ((100 - xc.discount) / 100)) with_discount
+     DECODE(xc.discount,0, xo.total_amount, xo.total_amount * ((100 - xc.discount) / 100)) with_discount,
       xc.discount	
 from xx_orders xo,
      xx_customers xc
